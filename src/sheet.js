585699333f8a3cf7bc620edbb3e39cd0ca29867c
Bill Garrett <garrett@astralfrontier.org>
2023-03-23 22:25:27 -0700

    Clean up saving throws

--- src/sheet.js
+++ src/sheet.js
@@ -1,34 +1,43 @@
 // Documentation: https://wiki.roll20.net/Sheet_Worker_Scripts
 
+function finishSkillRoll(outcome) {
+  const {
+    rollId,
+    results: {
+      roll: { dice, expression },
+    },
+  } = outcome
+  const [_dicestmt, diff] = expression.split('>')
+  const hits = dice.reduce(
+    (hits, die) => hits + (die >= diff ? 1 : 0) + (die == 20 ? 1 : 0),
+    0
+  )
+  let message
+  if (hits == 0) {
+    message = 'FAIL'
+  } else if (dice.includes(20)) {
+    message = `STRONG HIT (${hits})`
+  } else {
+    message = `HIT (${hits})`
+  }
+  finishRoll(rollId, {
+    diff: `${diff}`,
+    roll: dice.map((die) => `${die}`).join(' '),
+    message,
+  })
+}
+
 function startSkillRoll(skillname, skillrank) {
   startRoll(
     `&{template:check} {{name=${skillname}}} {{diff=[[0]]}} {{roll=[[${skillrank}d20>?{Difficulty|15}]]}} {{message=[[0]]}}`,
-    (outcome) => {
-      const {
-        rollId,
-        results: {
-          roll: { dice, expression },
-        },
-      } = outcome
-      const [_dicestmt, diff] = expression.split('>')
-      const hits = dice.reduce(
-        (hits, die) => hits + (die >= diff ? 1 : 0) + (die == 20 ? 1 : 0),
-        0
-      )
-      let message
-      if (hits == 0) {
-        message = 'FAIL'
-      } else if (dice.includes(20)) {
-        message = `STRONG HIT (${hits})`
-      } else {
-        message = `HIT (${hits})`
-      }
-      finishRoll(rollId, {
-        diff: `${diff}`,
-        roll: dice.map((die) => `${die}`).join(' '),
-        message,
-      })
-    }
+    finishSkillRoll
+  )
+}
+
+function startSaveRoll(status, difficulty) {
+  startRoll(
+    `&{template:save} {{name=${status}}} {{roll=[[1d20>${difficulty}]]}} {{message=[[0]]}}`,
+    finishSkillRoll
   )
 }
 
@@ -48,4 +57,13 @@ on('clicked:skilldefault', (event) => {
 
 on('clicked:trait', (event) => {
   startSkillRoll('Trait Reroll', 1)
+})
+
+on('clicked:save', (event) => {
+  const status = event.htmlAttributes['data-status-name']
+  const attrname = `status_save_${status}`
+  getAttrs([attrname], (v) => {
+    const diff = parseInt(v[attrname]) || 10
+    startSaveRoll(`${status.toUpperCase()}(${diff})`, diff)
+  })
 })
