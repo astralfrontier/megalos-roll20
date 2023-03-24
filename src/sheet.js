35b58a848627db33468379485f27b0caf6d2c28a
Bill Garrett <garrett@astralfrontier.org>
2023-03-23 20:41:28 -0700

    Support custom roll processing for skills

--- src/sheet.js
+++ src/sheet.js
@@ -1,1 +1,47 @@
 // Documentation: https://wiki.roll20.net/Sheet_Worker_Scripts
+
+function startSkillRoll(skillname, skillrank) {
+  startRoll(
+    `&{template:check} {{name=${skillname}}} {{diff=[[0]]}} {{roll=[[${skillrank}d20>?{Difficulty|15}]]}} {{message=[[0]]}}`,
+    (outcome) => {
+      const {
+        rollId,
+        results: {
+          roll: { dice, expression },
+        },
+      } = outcome
+      const [_dicestmt, diff] = expression.split('>')
+      const hits = dice.reduce(
+        (hits, die) => hits + (die >= diff ? 1 : 0) + (die == 20 ? 1 : 0),
+        0
+      )
+      let message
+      if (hits == 0) {
+        message = 'FAIL'
+      } else if (dice.includes(20)) {
+        message = `STRONG HIT (${hits})`
+      } else {
+        message = `HIT (${hits})`
+      }
+      finishRoll(rollId, {
+        diff: `${diff}`,
+        roll: dice.map((die) => `${die}`).join(' '),
+        message,
+      })
+    }
+  )
+}
+
+on('clicked:repeating_skills:skill', (event) => {
+  const { sourceAttribute } = event
+  // SourceAttribute: repeating_skills_-NRFjaNa3LW-sC6ntcBd_skill
+  getAttrs([`${sourceAttribute}name`, `${sourceAttribute}rank`], (v) => {
+    const skillname = v[`${sourceAttribute}name`]
+    const skillrank = v[`${sourceAttribute}rank`]
+    startSkillRoll(skillname, skillrank)
+  })
+})
+
+on('clicked:skilldefault', (event) => {
+  startSkillRoll('Untrained Skill', 1)
+})
