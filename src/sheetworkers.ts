f7296886aeb60494febc64c3631a07bb7fe42e0f
Bill Garrett <garrett@astralfrontier.org>
2023-04-08 01:53:49 -0700

    Include Barrier in soak rolls

--- src/sheetworkers.ts
+++ src/sheetworkers.ts
@@ -236,7 +236,7 @@ on('clicked:soak', (event) => {
     const dice = v['role'] == 'Tank' ? 2 : 1
     const diestring = modifiedDiceCount(dice, 6, v, true)
     startRoll(
-      `&{template:soak} {{name=${label}}} {{roll=[[${diestring}+@{${attr_name}}]]}}`,
+      `&{template:soak} {{name=${label}}} {{roll=[[${diestring}+@{${attr_name}}]]}} {{barrier=[[@{barrier}]]}}`,
       (outcome) => {
         finishRoll(outcome.rollId, {})
       }
