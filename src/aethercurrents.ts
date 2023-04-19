f7afc9b3322b6a00e221411803e12b688cc5d0fb
Bill Garrett <garrett@astralfrontier.org>
2023-04-19 08:59:57 -0700

    Show weak vs. surging charge on Cantrips

--- src/aethercurrents.ts
+++ src/aethercurrents.ts
@@ -1,11 +1,15 @@
-on('change:unlock_ac5', () => {
-  getSectionIDs('repeating_powers', (idarray) => {
-    getAttrs(['unlock_ac5'], (v) => {
-      const output: any = {}
-      idarray.forEach((id) => {
-        output[`repeating_powers_${id}_unlock_ac5`] = v.unlock_ac5
+on(
+  'change:unlock_ac5 change:aether_current_1 change:aether_current_2 change:aether_current_3 change:aether_current_4 change:aether_current_5',
+  (event) => {
+    const attr = event.sourceAttribute || ''
+    getSectionIDs('repeating_powers', (idarray) => {
+      getAttrs([attr], (v) => {
+        const output: any = {}
+        idarray.forEach((id) => {
+          output[`repeating_powers_${id}_${attr}`] = v[attr]
+        })
+        setAttrs(output)
       })
-      setAttrs(output)
     })
-  })
-})
+  }
+)
