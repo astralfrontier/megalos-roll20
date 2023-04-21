000b5b42f766932b595aca1c0fd33ab7b0a92bf4
Bill Garrett <garrett@astralfrontier.org>
2023-04-21 03:27:41 -0700

    #58: consolidate calculations into a single file

--- src/sheetworkers/calculations.ts
+++ src/sheetworkers/calculations.ts
@@ -89,3 +89,26 @@ on(
   )}`,
   update_condition_tracker
 )
+
+const aethercurrents_attributes = [
+  'unlock_ac5',
+  ...[1, 2, 3, 4, 5].map((n) => `aether_current_${n}`),
+]
+on(aethercurrents_attributes.map((s) => `change:${s}`).join(' '), (event) => {
+  const attr = event.sourceAttribute || ''
+  getAttrs(aethercurrents_attributes, (v) => {
+    const output: AttributeBundle = {}
+    const throne_damage = Object.keys(v).reduce(
+      (total, key) => total + (v[key] == 'Throne Damage' ? 1 : 0),
+      0
+    )
+    output['throne_damage'] = `${throne_damage}`
+    getSectionIDs('repeating_powers', (idarray) => {
+      // Update
+      idarray.forEach((id) => {
+        output[`repeating_powers_${id}_${attr}`] = v[attr]
+      })
+      setAttrs(output)
+    })
+  })
+})
